40f7829776427c53569b487eaf7e6b6a911c591e
Lucas Castelli <Lucas@Lucass-MacBook-Pro-2.local>
2021-12-03 12:09:00 -0600

    set up server and index, starting middleware

--- api/projects/projects-middleware.js
+++ api/projects/projects-middleware.js
@@ -1,1 +1,15 @@
 // add middlewares here related to projects
+const Projects = require('./projects-model');
+
+function errorHandling(err, req, res, next) {
+    res.status(err.status || 500).json({
+        message: ` There was an error ${err.message} `,
+        stack: err.stack
+    });
+}
+
+function verifyId(req, res, next)
+
+module.exports = {
+    errorHandling,
+}
--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -1,1 +1,23 @@
 // Write your "projects" router here!
+
+const { Router } = require('express');
+const express = require('express');
+
+// import middleware at some point
+
+const Projects = require('./projects-model')
+const router = express(Router);
+
+//Verify ID, Verify Body, errorHandle, 
+
+// GET /api/projects returns projects
+
+//GET /api/projects/id gets by ID
+
+// POST /api/projects returns new project as body
+
+// PUT /api/projects/id updates project as body of response 
+
+// DELETE /api/projects/id deletes, returns no response body
+
+// GET /api/projects/id/actions returns actions of specific ID
